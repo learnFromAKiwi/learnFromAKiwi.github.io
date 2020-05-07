e141c22f6c381463bc7f02876c715feff426b902
Ryan Tan <rtan265@aucklanduni.ac.nz>
2020-05-07 17:16:00 +1200

    Updates

---
+++ precache-manifest.0741fd253088b8767bc837b8bfe6bb01.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adcd946b2babf3969dcf9f20582dcdbb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55ad005bc441db4de366",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "ea9225ea7be759938a06",
+    "url": "/static/css/main.11aaa2da.chunk.css"
+  },
+  {
+    "revision": "55ad005bc441db4de366",
+    "url": "/static/js/2.d6c6beb7.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.d6c6beb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea9225ea7be759938a06",
+    "url": "/static/js/main.775f0dfd.chunk.js"
+  },
+  {
+    "revision": "bce316a6c8dfbe473433",
+    "url": "/static/js/runtime-main.607fe6fa.js"
+  },
+  {
+    "revision": "d5f0458a172019946a6565ed8c0ddab0",
+    "url": "/static/media/brand.d5f0458a.png"
+  },
+  {
+    "revision": "9a6d9f54ef24fe5397dc66c8aff1592f",
+    "url": "/static/media/cassieBen.9a6d9f54.jpg"
+  },
+  {
+    "revision": "84faf9969ff8c1bc366305253be4aabe",
+    "url": "/static/media/esther.84faf996.jpg"
+  },
+  {
+    "revision": "30e2c502c02099758298a1d02292818c",
+    "url": "/static/media/exploreTimetableJumbotron.30e2c502.png"
+  },
+  {
+    "revision": "6897e3d71e24f727501faea2aa3f447c",
+    "url": "/static/media/instructorJumbotron.6897e3d7.jpg"
+  },
+  {
+    "revision": "36529bfe6a212813fa0222858fa70865",
+    "url": "/static/media/janine.36529bfe.jpg"
+  },
+  {
+    "revision": "b87f582c565be3ec0464149ae86a8cb2",
+    "url": "/static/media/jumbotron.b87f582c.jpg"
+  },
+  {
+    "revision": "6049f0957959099fad2f37b22c7035a3",
+    "url": "/static/media/landingPageMomJumbo.6049f095.png"
+  },
+  {
+    "revision": "c1c9b4e81a204d377feefd901eeacbe3",
+    "url": "/static/media/landingPageYoga.c1c9b4e8.png"
+  },
+  {
+    "revision": "3abed537b6405a5db8252643fcca786b",
+    "url": "/static/media/li.3abed537.jpg"
+  },
+  {
+    "revision": "5610c175773a78bbf2327469cf5597f7",
+    "url": "/static/media/mariaDeclan.5610c175.jpg"
+  },
+  {
+    "revision": "54d19239783d67098db4063f7555c3f9",
+    "url": "/static/media/max.54d19239.jpg"
+  },
+  {
+    "revision": "369899c0da540aee86cf4f47c5044409",
+    "url": "/static/media/nick.369899c0.jpg"
+  },
+  {
+    "revision": "5b34429f7dcc9a81f6d8d697c2bd18e1",
+    "url": "/static/media/selena.5b34429f.jpg"
+  },
+  {
+    "revision": "369899c0da540aee86cf4f47c5044409",
+    "url": "/static/media/taryn.369899c0.jpg"
+  }
+]);
